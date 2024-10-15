113ba562325b09d1cdbd9d5feecfe537d0600106
Daybreak312 <ty82afg12@gmail.com>
2024-10-15 11:25:36 +0900

    241015

--- 241015/ex10p04.c
+++ 241015/ex10p04.c
@@ -9,19 +9,20 @@ int handled = 0;
 void handle(int signum) {
     if (signum == SIGINT) {
         printf("[parent] Got signal.\n");
+        handled = 1;
     }
-    handled = 1;
 }
 
 int main() {
     pid_t pid;
     int count = 5;
 
-    if ((pid = fork()) > 0) {
-        static struct sigaction act;
+    static struct sigaction act;
 
-        act.sa_handler = handle;
-        sigfillset(&(act.sa_mask));
+    act.sa_handler = handle;
+    sigfillset(&(act.sa_mask));
+
+    if ((pid = fork()) > 0) {
 
         while (!handled) {
             sleep(1);
