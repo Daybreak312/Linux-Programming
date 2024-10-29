63f46d84aa855c60bd6935510598ddc595f1d4e3
Daybreak312 <ty82afg12@gmail.com>
2024-10-29 10:10:06 +0900

    241029

---
+++ 241029/ex11p01.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+#define SIZE 512
+
+int main() {
+    pid_t pid1, pid2;
+
+    char buffer[SIZE];
+    int filedes1[2], filedes2[2];
+
+    if (pipe(filedes1) == -1) {
+        printf("call pipe failed #1\n");
+        exit(1);
+    }
+
+    if ((pid1 = fork()) > 0) {
+
+        filedes1[1] = 0;
+
+        sleep(5);
+
+        read(filedes1[0], buffer, SIZE);
+
+        printf("[first] read finish!: %s\n", buffer);
+
+    } else if (pid1 == 0) {
+
+        if (pipe(filedes2) == -1) {
+            printf("call pipe failed #2\n");
+            exit(1);
+        }
+
+        if ((pid2 = fork()) > 0) {
+
+            filedes2[1] = 0;
+
+            sleep(5);
+
+            read(filedes2[0], buffer, SIZE);
+
+            printf("[second] read finish!: %s\n", buffer);
+
+        } else if (pid2 == 0) {
+
+            filedes1[0] = filedes2[0] = 0;
+
+            strcpy(buffer, "wow i'm hacker!!\n");
+
+            write(filedes1[1], buffer, SIZE);
+            write(filedes2[1], buffer, SIZE);
+
+            printf("[third] write finish. bye!\n");
+
+        } else {
+            printf("fork failed: #2\n");
+        }
+
+    } else {
+        printf("fork failed: #1\n");
+    }
+}
